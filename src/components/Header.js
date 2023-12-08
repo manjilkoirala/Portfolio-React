122b1b173ad111791cc577ab61305679b3d7ec44
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2023-12-08 19:27:27 +0545

    added link to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,15 @@ import {Link} from 'react-router-dom'
 const Header = () => {
   return (
     <nav className="flex items-center justify-between px-6 md:px-28 h-16">
+      <Link to="/">
       <div className=" w-10">
         <img src={logo} alt="logo" />
-      </div>
+      </div></Link>
       <div className="flex gap-8 items-center text-gray-700 text-xl">
        
-        <Link to="/" className=" hover:text-black">Home</Link>
-        <Link to="/Projects" className=" hover:text-black">Project</Link>
-        <Link to="/Contact" className=" hover:text-black">Contact</Link>
+        <Link to="/" className=" nav hover:text-black">Home</Link>
+        <Link to="/Projects" className="nav hover:text-black">Project</Link>
+        <Link to="/Contact" className="nav hover:text-black">Contact</Link>
       </div>
     </nav>
   );
